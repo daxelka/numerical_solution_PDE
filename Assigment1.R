698febc3e5775172ac24c3b2a78170596a098320
Alina <daxelka@yandex.ru>
2015-10-19 23:20:46 +0100

    extract generic equation methods to separate object

--- Assigment1.R
+++ Assigment1.R
@@ -8,51 +8,19 @@ library(GetoptLong)
 # N - discratization parameter
 # difference scheme ('central' or 'bakwards')
 
-PDEsolution <- function(a, b, N, scheme = "central") { 
-	
-	#1 numerical solution________________________________________
-	
-	#computed parameters
-	#step
-	h <- 1/N
-	
+Equation <- function(a,b) {
+		
 	#boundary conditions
 	b1 <- 1; b2 <- 0
-	 	
-	# creating right-hand side vector vF
-	xi <- seq(0, 1, by = h)			# vector with xi, i = 1...N+1 
-    vF <- c(b1, xi[2:N]^2 ,b2)		# right-hand side vector f(x)
-		
-	# Difference scheme's coefficients
-		if (scheme =="central") {
-			k1 <- -a/(h^2) - b/(2*h); k2 <- 2*a/(h^2); k3 <- -a/(h^2)+b/(2*h)	
-		}	
-		if (scheme =="backward"){
-			k1 <- -a/(h^2) - b/h; k2 <- 2*a/(h^2)+b/h; k3 <- -a/(h^2)	
-		}	
-	# creating tridiagonal matrix mA
-		d1 <- c(rep(k1,N-1),0)		# subdiagonal vector with length = N-1
-		d2 <- c(1,rep(k2,N-1),1)	# diagonal vector with length = N
-		d3 <- c(0,rep(k3,N-1))		# superdiagonal vector with length  = N-1
-
-		diag.num <- -outer(seq(d2),seq(d2),"-")
-		mA <- diag(d2)
-		mA[diag.num == 1] <- d3
-		mA[diag.num == -1] <- d1	
-
-
-	# solving linear system of equations mA %*% vU =vF
-		vU <-  solve(mA, vF)
-
-	#2 exact solution___________________________________________________
 	
+	#righthand side function
+	RightHand <- function(xi) { xi^2 }
+	
+	ExactSolution <- function(xi) {
 		if (b == 0) {
 			u <- -(xi^4)/(12*a) - (1-1/(12*a))*xi + 1 			
-		}
-		else{
-			#c1 <- (-1/(3*b) - a/(b^2) - (2*a^2)/(b^3) - 1)/(exp(b/a)-1)
-			#c2 <- 1 - c1
-			#u <- c1*exp(xi*b/a) + c2 + (xi^3)/(3*b) + (a*xi^2)/(b^2) + (2*xi*a^2)/(b^3)
+		} 
+		else {
 			c1 <- (-1/(3*b) - a/(b^2) - (2*a^2)/(b^3) - 1)/(1-exp(-b/a))
 			c2 <- 1 - (-1/(3*b) - a/(b^2) - (2*a^2)/(b^3) - 1)*exp(-b/a)/(1-exp(-b/a))
 			c3 <- 1/(3*b)
@@ -61,12 +29,68 @@ PDEsolution <- function(a, b, N, scheme = "central") {
 			
 			u <- c1*exp((xi-1)*b/a) + c2 + c3*xi^3 + c4*xi^2 + c5*xi
 		}
+	}
 	
-	result <- data.frame(u,vU,xi)
+	# Difference scheme's coefficients
+	# k1*U_{i-1} +k2*U_i + k3*U_{i+1} = f(x_i)
+	DiffSchemeCoeff <- function(h, scheme) {
+		if (scheme =="central") {
+			list(k1 = -a/(h^2) - b/(2*h),
+				 k2 = 2*a/(h^2),
+				 k3 = -a/(h^2) + b/(2*h))
+		}	
+		else if (scheme =="backward"){
+			list(k1 = -a/(h^2) - b/h,
+				 k2 = 2*a/(h^2)+b/h,
+				 k3 = -a/(h^2))
+		}		
+	}
+	
+		
+	out <- list(boundary1 = b1, 
+				boundary2 = b2, 
+				right_hand = RightHand,
+				exact_solution = ExactSolution,
+				diff_scheme_coeff = DiffSchemeCoeff)
+	
+	return(out)
+}
+
+
+PDEsolution <- function(a, b, N, scheme = "central") { 
+	equation <- Equation(a, b) 
+	#1 numerical solution________________________________________
+	
+	#computed parameters
+	#step
+	h <- 1/N
+	
+	# creating right-hand side vector vF
+	xi <- seq(0, 1, by = h)			# vector with xi, i = 1...N+1 
+    vF <- c(equation$boundary1, equation$right_hand(xi[2:N]), equation$boundary2)		# right-hand side vector f(x)
+	
+	# coefficients of difference scheme
+	coefficients <- equation$diff_scheme_coeff(h, scheme)	
+		
+	# creating tridiagonal matrix mA
+		d1 <- c(   rep(coefficients$k1, N-1), 0)		# subdiagonal vector with length = N-1
+		d2 <- c(1, rep(coefficients$k2, N-1), 1)	    # diagonal vector with length = N
+		d3 <- c(0, rep(coefficients$k3, N-1)   )		# superdiagonal vector with length  = N-1
+
+		diag.num <- -outer(seq(d2), seq(d2),"-")
+		mA <- diag(d2)
+		mA[diag.num == 1] <- d3
+		mA[diag.num == -1] <- d1	
+
+
+	# solving linear system of equations mA %*% vU =vF
+		vU <-  solve(mA, vF)
+
+	result <- data.frame(equation$exact_solution(xi), vU, xi)
 	names(result) <- c("exact","numeric","mesh")
 	
 	error = max(abs(result$exact - result$numeric))
-	out <- list(data=result, error=error, a=a, b=b, N=N, scheme = scheme)
+	out <- list(data=result, error=error, a=a, b=b, N=N, scheme=scheme)
 	
 	class(out) <- "PDEsolution"
 	
@@ -74,7 +98,7 @@ PDEsolution <- function(a, b, N, scheme = "central") {
 }
 
 print.PDEsolution <- function(object) {
-	cat("Max. error: ", object$error, "\n")
+	cat("Max. error: ", object$error, "\n\n")
 	print(object$data)
 }
 
@@ -87,6 +111,8 @@ plot.PDEsolution <- function(object) {
 }
 
 # parameters variation__________________
+PDEsolution(1,0,16,"central")
+
 par(mfrow=c(2,2))
 plot(PDEsolution(1e-3,1,16,"central"))
 plot(PDEsolution(1e-3,1,128,"central"))
